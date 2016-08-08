176f7925b82a9a7d95900f6f95b6356f2eed99db
Benjamin Alan Weaver <baweaver@lbl.gov>
2016-08-08 14:02:50 -0700

    add library files

---
+++ js/index.js
@@ -0,0 +1,18 @@
+$(function() {
+  var IPinfo, displayIP, onDataReceived;
+  IPinfo = {
+    ip: '::1',
+    hostname: 'localhost'
+  };
+  onDataReceived = function(data) {
+    return IPinfo = data;
+  };
+  displayIP = function() {
+    $('#IPAddress').html(IPinfo.ip);
+    $('#HostName').html(IPinfo.hostname);
+    $('#userAgent').html(navigator.userAgent);
+    return true;
+  };
+  $.getJSON("http://ipinfo.io", {}, onDataReceived).error(displayIP).complete(displayIP);
+  return true;
+});
