16e92d3720157a5ae2684402ebf9aa3e5f622dcd
Benjamin Alan Weaver <baweaver@lbl.gov>
2018-05-16 22:53:21 -0700

    working qso plot

--- js/qso.coffee
+++ js/qso.coffee
@@ -1,8 +1,75 @@
 $( () ->
     qso = []
-    replot -> ()
+    meta =
+        id: ""
+        targetid: ""
+        ra: ""
+        dec: ""
+        redshift: ""
+        zerr: ""
+        zwarn: ""
+        type: ""
+        subtype: ""
+
+    replot = () ->
+        $("#targetid").html(meta.targetid)
+        $("#ra").html(meta.ra)
+        $("#dec").html(meta.dec)
+        $("#redshift").html(meta.redshift + " &pm; " + meta.zerr)
+        $("#zwarn").html(meta.zwarn)
+        $("#type").html(meta.type)
+        $("#subtype").html(meta.subtype)
+        # options =
+        #     labels: ["Wavelength", "b"]
+        #     xlabel: "Wavelength [Å]"
+        #     ylabel: "Flux [10<sup>-17</sup> erg cm<sup>-2</sup> s<sup>-1</sup> Å<sup>-1</sup>]"
+        #     # valueRange: [0, 100]
+        #     # connectSeparatedPoints: true
+        #     series:
+        #         b:
+        #             color: "blue"
+        #             # errorBars: true
+        # g = new Dygraph document.getElementById("spectrum"), qso, options
+        options =
+            axisLabels:
+                show: true
+            xaxis:
+                axisLabel: "Wavelength [Å]"
+            yaxis:
+                axisLabel: "Flux [10<sup>-17</sup> erg cm<sup>-2</sup> s<sup>-1</sup> Å<sup>-1</sup>]"
+            zoom:
+                interactive: true
+            pan:
+                interactive: true
+            series:
+                shadowSize: 0
+        p = $.plot $("#spectrum"), qso, options
         true
-    onDataReceived -> (data)
+
+    onDataReceived = (data) ->
+        for own key, value of meta
+            meta[key] = data[key]
+        qso = [
+            {data: [], color: 'blue', label: 'b', lines: {lineWidth: 1, fill: false}, opacity: 0.5},
+            {data: [], color: 'red', label: 'r', lines: {lineWidth: 1, fill: false}, opacity: 0.5},
+            {data: [], color: 'black', label: 'z', lines: {lineWidth: 1, fill: false}, opacity: 0.5},
+            {data: [], color: 'blue', id: 'upper_b', lines: {lineWidth: 0, fill: 0.3}, fillBetween: 'lower_b'},
+            {data: [], color: 'red', id: 'upper_r', lines: {lineWidth: 0, fill: 0.3}, fillBetween: 'lower_r'},
+            {data: [], color: 'black', id: 'upper_z', lines: {lineWidth: 0, fill: 0.3}, fillBetween: 'lower_z'},
+            {data: [], color: 'blue', id: 'lower_b', lines: {lineWidth: 0, fill: false}},
+            {data: [], color: 'red', id: 'lower_r', lines: {lineWidth: 0, fill: false}},
+            {data: [], color: 'black', id: 'lower_z', lines: {lineWidth: 0, fill: false}}
+        ]
+        for s, i in ['b', 'r', 'z']
+            for w, j in data[s].wavelength
+                if data[s].ivar[i] > 0
+                    std = 1.0 / Math.sqrt(data[s].ivar[j])
+                    # qso.push [w, [data[s].flux[j], std]]
+                    qso[i].data.push [w, data[s].flux[j]]
+                    qso[i+3].data.push [w, data[s].flux[j] + std]
+                    qso[i+6].data.push [w, data[s].flux[j] - std]
+                # else
+                #     qso.push [w, null]
         true
     if qso.length == 0
         $.getJSON('lib/qso.json', {}, onDataReceived).error( () -> alert("Data retrieval error!") ).complete(replot)
--- js/qso.js
+++ js/qso.js
@@ -1,8 +1,158 @@
+var hasProp = {}.hasOwnProperty;
+
 $(function() {
-  onDataReceived(function() {
-    return data;
-  });
-  return $.getJSON('lib/qso.json', {}, onDataReceived).error(function() {
-    return alert("Data retrieval error!");
-  }).complete(replot);
+  var meta, onDataReceived, qso, replot;
+  qso = [];
+  meta = {
+    id: "",
+    targetid: "",
+    ra: "",
+    dec: "",
+    redshift: "",
+    zerr: "",
+    zwarn: "",
+    type: "",
+    subtype: ""
+  };
+  replot = function() {
+    var options, p;
+    $("#targetid").html(meta.targetid);
+    $("#ra").html(meta.ra);
+    $("#dec").html(meta.dec);
+    $("#redshift").html(meta.redshift + " &pm; " + meta.zerr);
+    $("#zwarn").html(meta.zwarn);
+    $("#type").html(meta.type);
+    $("#subtype").html(meta.subtype);
+    options = {
+      axisLabels: {
+        show: true
+      },
+      xaxis: {
+        axisLabel: "Wavelength [Å]"
+      },
+      yaxis: {
+        axisLabel: "Flux [10<sup>-17</sup> erg cm<sup>-2</sup> s<sup>-1</sup> Å<sup>-1</sup>]"
+      },
+      zoom: {
+        interactive: true
+      },
+      pan: {
+        interactive: true
+      },
+      series: {
+        shadowSize: 0
+      }
+    };
+    p = $.plot($("#spectrum"), qso, options);
+    return true;
+  };
+  onDataReceived = function(data) {
+    var i, j, k, key, l, len, len1, ref, ref1, s, std, value, w;
+    for (key in meta) {
+      if (!hasProp.call(meta, key)) continue;
+      value = meta[key];
+      meta[key] = data[key];
+    }
+    qso = [
+      {
+        data: [],
+        color: 'blue',
+        label: 'b',
+        lines: {
+          lineWidth: 1,
+          fill: false
+        },
+        opacity: 0.5
+      }, {
+        data: [],
+        color: 'red',
+        label: 'r',
+        lines: {
+          lineWidth: 1,
+          fill: false
+        },
+        opacity: 0.5
+      }, {
+        data: [],
+        color: 'black',
+        label: 'z',
+        lines: {
+          lineWidth: 1,
+          fill: false
+        },
+        opacity: 0.5
+      }, {
+        data: [],
+        color: 'blue',
+        id: 'upper_b',
+        lines: {
+          lineWidth: 0,
+          fill: 0.3
+        },
+        fillBetween: 'lower_b'
+      }, {
+        data: [],
+        color: 'red',
+        id: 'upper_r',
+        lines: {
+          lineWidth: 0,
+          fill: 0.3
+        },
+        fillBetween: 'lower_r'
+      }, {
+        data: [],
+        color: 'black',
+        id: 'upper_z',
+        lines: {
+          lineWidth: 0,
+          fill: 0.3
+        },
+        fillBetween: 'lower_z'
+      }, {
+        data: [],
+        color: 'blue',
+        id: 'lower_b',
+        lines: {
+          lineWidth: 0,
+          fill: false
+        }
+      }, {
+        data: [],
+        color: 'red',
+        id: 'lower_r',
+        lines: {
+          lineWidth: 0,
+          fill: false
+        }
+      }, {
+        data: [],
+        color: 'black',
+        id: 'lower_z',
+        lines: {
+          lineWidth: 0,
+          fill: false
+        }
+      }
+    ];
+    ref = ['b', 'r', 'z'];
+    for (i = k = 0, len = ref.length; k < len; i = ++k) {
+      s = ref[i];
+      ref1 = data[s].wavelength;
+      for (j = l = 0, len1 = ref1.length; l < len1; j = ++l) {
+        w = ref1[j];
+        if (data[s].ivar[i] > 0) {
+          std = 1.0 / Math.sqrt(data[s].ivar[j]);
+          qso[i].data.push([w, data[s].flux[j]]);
+          qso[i + 3].data.push([w, data[s].flux[j] + std]);
+          qso[i + 6].data.push([w, data[s].flux[j] - std]);
+        }
+      }
+    }
+    return true;
+  };
+  if (qso.length === 0) {
+    return $.getJSON('lib/qso.json', {}, onDataReceived).error(function() {
+      return alert("Data retrieval error!");
+    }).complete(replot);
+  }
 });
