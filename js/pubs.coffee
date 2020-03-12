7217951736e35d7a43be94597a169795cf341192
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-03-12 13:18:23 -0700

    working for notices

--- js/pubs.coffee
+++ js/pubs.coffee
@@ -2,27 +2,34 @@ $( () ->
     #
     #
     #
-    Publications =
+    P = {}
         #
         #
         #
-        data: {}
-        #
-        #
-        #
-        display: () ->
-            if @data.astronomerstelegram?
-                alert @data.astronomerstelegram.url.replace(/%d/, @data.astronomerstelegram.data[0])
-            true
-    #
-    #
-    #
-    onDataReceived = (data) ->
-        Publications.data = data
+    display = () ->
+        if P.astronomerstelegram?
+            p = $('#astronomerstelegram')
+            p.empty()
+            a = []
+            for d in P.astronomerstelegram.data
+                u = P.astronomerstelegram.url.replace(/%d/, d)
+                a.push "<a href=\"#{u}\">#{d}</a>"
+            p.html a.join ", "
+        if P.cbet?
+            p = $('#cbet')
+            p.empty()
+            a = []
+            for d in P.cbet.data
+                d6 = if d < 1000 then "000#{d}" else "00#{d}"
+                d1 = if d < 1000 then Math.floor(d/100)*100 else Math.floor(d/1000)*1000
+                z = if d < 1000 then "000#{d1}" else "00#{d1}"
+                u = P.cbet.url.replace(/%06d/, d6).replace(/%s/, z)
+                a.push "<a href=\"#{u}\">#{d}</a>"
+            p.html a.join ", "
         true
     #
     #
     #
-    if $.isEmptyObject(Publications.data)
-        $.getJSON('pubs.json', {}, onDataReceived).fail( () -> alert("Data retrieval error!") ).done(Publications.display)
+    if $.isEmptyObject P
+        $.getJSON('pubs.json', {}, (data) -> P = data).fail( () -> alert("Data retrieval error!") ).done(display)
 )
--- js/pubs.js
+++ js/pubs.js
@@ -1,21 +1,42 @@
 $(function() {
-  var Publications, onDataReceived;
-  Publications = {
-    data: {},
-    display: function() {
-      if (this.data.astronomerstelegram != null) {
-        alert(this.data.astronomerstelegram.url.replace(/%d/, this.data.astronomerstelegram.data[0]));
+  var P, display;
+  P = {};
+  display = function() {
+    var a, d, d1, d6, i, j, len, len1, p, ref, ref1, u, z;
+    if (P.astronomerstelegram != null) {
+      p = $('#astronomerstelegram');
+      p.empty();
+      a = [];
+      ref = P.astronomerstelegram.data;
+      for (i = 0, len = ref.length; i < len; i++) {
+        d = ref[i];
+        u = P.astronomerstelegram.url.replace(/%d/, d);
+        a.push("<a href=\"" + u + "\">" + d + "</a>");
       }
-      return true;
+      p.html(a.join(", "));
     }
-  };
-  onDataReceived = function(data) {
-    Publications.data = data;
+    if (P.cbet != null) {
+      p = $('#cbet');
+      p.empty();
+      a = [];
+      ref1 = P.cbet.data;
+      for (j = 0, len1 = ref1.length; j < len1; j++) {
+        d = ref1[j];
+        d6 = d < 1000 ? "000" + d : "00" + d;
+        d1 = d < 1000 ? Math.floor(d / 100) * 100 : Math.floor(d / 1000) * 1000;
+        z = d < 1000 ? "000" + d1 : "00" + d1;
+        u = P.cbet.url.replace(/%06d/, d6).replace(/%s/, z);
+        a.push("<a href=\"" + u + "\">" + d + "</a>");
+      }
+      p.html(a.join(", "));
+    }
     return true;
   };
-  if ($.isEmptyObject(Publications.data)) {
-    return $.getJSON('pubs.json', {}, onDataReceived).fail(function() {
+  if ($.isEmptyObject(P)) {
+    return $.getJSON('pubs.json', {}, function(data) {
+      return P = data;
+    }).fail(function() {
       return alert("Data retrieval error!");
-    }).done(Publications.display);
+    }).done(display);
   }
 });
