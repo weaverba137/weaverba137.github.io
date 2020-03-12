7ae3d23c7ccbb951dfe6c3962a754f2df9e3a7d9
Benjamin Alan Weaver <baweaver@lbl.gov>
2020-03-12 15:48:49 -0700

    move more pubs to json

--- js/pubs.coffee
+++ js/pubs.coffee
@@ -7,25 +7,42 @@ $( () ->
         #
         #
     display = () ->
-        if P.astronomerstelegram?
-            p = $('#astronomerstelegram')
-            p.empty()
+        for own k, v of P.notices
+            div = $("##{k}")
+            div.empty()
+            h3 = $("<h3/>").html(v.title).appendTo div
             a = []
-            for d in P.astronomerstelegram.data
-                u = P.astronomerstelegram.url.replace(/%d/, d)
+            for d in v.data
+                if k == "cbet"
+                    d6 = if d < 1000 then "000#{d}" else "00#{d}"
+                    d1 = if d < 1000 then Math.floor(d/100)*100 else Math.floor(d/1000)*1000
+                    z = if d < 1000 then "000#{d1}" else "00#{d1}"
+                    u = v.url.replace(/%06d/, d6).replace(/%s/, z)
+                else
+                    u = v.url.replace(/%d/, d)
                 a.push "<a href=\"#{u}\">#{d}</a>"
-            p.html a.join ", "
-        if P.cbet?
-            p = $('#cbet')
-            p.empty()
-            a = []
-            for d in P.cbet.data
-                d6 = if d < 1000 then "000#{d}" else "00#{d}"
-                d1 = if d < 1000 then Math.floor(d/100)*100 else Math.floor(d/1000)*1000
-                z = if d < 1000 then "000#{d1}" else "00#{d1}"
-                u = P.cbet.url.replace(/%06d/, d6).replace(/%s/, z)
-                a.push "<a href=\"#{u}\">#{d}</a>"
-            p.html a.join ", "
+            p = $("<p/>").html(a.join ", ").appendTo div
+        for own k, v of P.other
+            div = $("##{k}")
+            div.empty()
+            h3 = $("<h3/>").html(v.title).appendTo div
+            for r in v.data
+                if r.author[r.author.length-1] == "et al."
+                    r.author[r.author.length-1] = "<em>et al.</em>"
+                authors = (au.replace(/ +/g, "&nbsp;") for au in r.author).join(", ")
+                title = "&ldquo;#{r.title}&rdquo;"
+                h = [authors, title]
+                if k == "arXiv"
+                    u = "https://arxiv.org/abs/#{r.id}"
+                    h.push "#{k}:#{r.id}"
+                else
+                    u = "https://ui.adsabs.harvard.edu/abs/#{r.id}/abstract"
+                    j = r.journal.replace(/ +/g, "&nbsp;")
+                    h.push "<em>#{j}</em> <strong>#{r.volume}</strong> (#{r.year}) #{r.pages}"
+                p = $("<p/>").addClass("pub")
+                a = $("<a/>").attr("href", u).html(h.join(", ") + ".")
+                a.appendTo p
+                p.appendTo div
         true
     #
     #
--- js/pubs.js
+++ js/pubs.js
@@ -1,34 +1,71 @@
+var hasProp = {}.hasOwnProperty;
+
 $(function() {
   var P, display;
   P = {};
   display = function() {
-    var a, d, d1, d6, i, j, len, len1, p, ref, ref1, u, z;
-    if (P.astronomerstelegram != null) {
-      p = $('#astronomerstelegram');
-      p.empty();
+    var a, au, authors, d, d1, d6, div, h, h3, i, j, k, l, len, len1, p, r, ref, ref1, ref2, ref3, title, u, v, z;
+    ref = P.notices;
+    for (k in ref) {
+      if (!hasProp.call(ref, k)) continue;
+      v = ref[k];
+      div = $("#" + k);
+      div.empty();
+      h3 = $("<h3/>").html(v.title).appendTo(div);
       a = [];
-      ref = P.astronomerstelegram.data;
-      for (i = 0, len = ref.length; i < len; i++) {
-        d = ref[i];
-        u = P.astronomerstelegram.url.replace(/%d/, d);
+      ref1 = v.data;
+      for (i = 0, len = ref1.length; i < len; i++) {
+        d = ref1[i];
+        if (k === "cbet") {
+          d6 = d < 1000 ? "000" + d : "00" + d;
+          d1 = d < 1000 ? Math.floor(d / 100) * 100 : Math.floor(d / 1000) * 1000;
+          z = d < 1000 ? "000" + d1 : "00" + d1;
+          u = v.url.replace(/%06d/, d6).replace(/%s/, z);
+        } else {
+          u = v.url.replace(/%d/, d);
+        }
         a.push("<a href=\"" + u + "\">" + d + "</a>");
       }
-      p.html(a.join(", "));
+      p = $("<p/>").html(a.join(", ")).appendTo(div);
     }
-    if (P.cbet != null) {
-      p = $('#cbet');
-      p.empty();
-      a = [];
-      ref1 = P.cbet.data;
-      for (j = 0, len1 = ref1.length; j < len1; j++) {
-        d = ref1[j];
-        d6 = d < 1000 ? "000" + d : "00" + d;
-        d1 = d < 1000 ? Math.floor(d / 100) * 100 : Math.floor(d / 1000) * 1000;
-        z = d < 1000 ? "000" + d1 : "00" + d1;
-        u = P.cbet.url.replace(/%06d/, d6).replace(/%s/, z);
-        a.push("<a href=\"" + u + "\">" + d + "</a>");
+    ref2 = P.other;
+    for (k in ref2) {
+      if (!hasProp.call(ref2, k)) continue;
+      v = ref2[k];
+      div = $("#" + k);
+      div.empty();
+      h3 = $("<h3/>").html(v.title).appendTo(div);
+      ref3 = v.data;
+      for (l = 0, len1 = ref3.length; l < len1; l++) {
+        r = ref3[l];
+        if (r.author[r.author.length - 1] === "et al.") {
+          r.author[r.author.length - 1] = "<em>et al.</em>";
+        }
+        authors = ((function() {
+          var len2, m, ref4, results;
+          ref4 = r.author;
+          results = [];
+          for (m = 0, len2 = ref4.length; m < len2; m++) {
+            au = ref4[m];
+            results.push(au.replace(/ +/g, "&nbsp;"));
+          }
+          return results;
+        })()).join(", ");
+        title = "&ldquo;" + r.title + "&rdquo;";
+        h = [authors, title];
+        if (k === "arXiv") {
+          u = "https://arxiv.org/abs/" + r.id;
+          h.push(k + ":" + r.id);
+        } else {
+          u = "https://ui.adsabs.harvard.edu/abs/" + r.id + "/abstract";
+          j = r.journal.replace(/ +/g, "&nbsp;");
+          h.push("<em>" + j + "</em> <strong>" + r.volume + "</strong> (" + r.year + ") " + r.pages);
+        }
+        p = $("<p/>").addClass("pub");
+        a = $("<a/>").attr("href", u).html(h.join(", ") + ".");
+        a.appendTo(p);
+        p.appendTo(div);
       }
-      p.html(a.join(", "));
     }
     return true;
   };
